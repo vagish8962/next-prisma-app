dfbd8c34ffe800799d67d9381d0e72afa7653260
gupta, Vagish <Vagish.gupta@netapp.com>
2023-07-13 10:48:00 +0530

    First commit

---
+++ app/components/nav/UserMenu.jsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { useCallback, useState } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { useRouter } from "next/navigation";
+
+import MenuItem from "./MenuItem";
+
+function UserMenu() {
+  const router = useRouter();
+
+  return ( 
+    <div className="relative">
+        hello
+    </div>
+   );
+}
+ 
+export default UserMenu;
